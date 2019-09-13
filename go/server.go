04e51d4bae57860f64f1f2b3d53dbd2dcbb03d52
naofumi-fujii <naofumi.fujii@gmail.com>
2019-09-13 15:46:04 +0900

    .

--- go/server.go
+++ go/server.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%#v", r)
 	fmt.Fprintf(w, "Hello, World")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	httpPort := 8080
+	fmt.Printf("listening on %v\n", httpPort)
+	http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
 }
