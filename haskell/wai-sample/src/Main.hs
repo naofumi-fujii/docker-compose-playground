5b50265a625c08c9e2d13522ce3746bc346eae26
naofumi-fujii <naofumi.fujii@gmail.com>
2019-12-12 17:07:13 +0900

    -import Data.ByteString.Char8(unpack)

--- haskell/wai-sample/src/Main.hs
+++ haskell/wai-sample/src/Main.hs
@@ -2,7 +2,6 @@
 module Main where
 
 import Data.ByteString.Builder (byteString)
-import Data.ByteString.Char8(unpack)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString.Char8 as BS
 import Network.HTTP.Types (status200)
@@ -23,7 +22,7 @@ main = do
 logApp :: ApacheLogger -> Application
 logApp aplogger req response = do
     rbc <- getRequestBodyChunk(req)
-    putStrLn(unpack(rbc))
+    putStrLn(BS.unpack(rbc))
     liftIO $ aplogger req status (Just len)
     response $ responseBuilder status hdr msg
   where
